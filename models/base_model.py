f643435293136f590a270b2b287b23a83b45f66d
rayenhdr93 <hedrirayen69@gmail.com>
2022-03-01 18:22:40 +0100

    to_dict

--- models/base_model.py
+++ models/base_model.py
@@ -10,14 +10,17 @@ class BaseModel:
     def __init__(self):
         '''__init__'''
         self.id = str(uuid.uuid4())
-        self.created_at = datetime.today()
-        self.updated_at = datetime.today()
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
     def __str__(self):
         '''__str__'''
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+        return "[{}] ({}) {}".format(self.__class__.__name__,
+                                     self.id, self.__dict__)
 
     def save(self):
         '''save'''
-        self.updated_at = datetime.today()
+        self.updated_at = datetime.now()
 
+    def to_dict(self):
+        return self.__dict__
