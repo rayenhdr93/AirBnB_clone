284fef5ed945e0e2c6e93f640c08e75d07b238fd
Aymenbs22 <aymenbenslima1@gmail.com>
2022-03-03 02:57:19 -0800

    f

--- models/base_model.py
+++ models/base_model.py
@@ -19,7 +19,9 @@ class BaseModel:
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
     def __str__(self):
         '''__str__'''
