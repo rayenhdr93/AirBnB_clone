d6d13128298dc8e7a8fd04255c72f219f2455db7
rayenhdr93 <hedrirayen69@gmail.com>
2022-03-05 16:40:59 +0100

    file storeage

---
+++ models/engine/file_storage.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Def BaseModel"""
+from models.base_model import BaseModel
+import json
+
+
+class FileStorage:
+    '''FileStorage'''
+    __file_path = 'objects.json'
+    __objects = {}
+    def all(self):
+        return (self.__objects)
+
+    def new(self, obj):
+        if obj:
+            self.__objects["{}.{}".format(BaseModel.__name__, BaseModel.id)] = obj
+
+    def save(self):
+        save_dict = {}
+        for key, value in self.__objects.items():
+            save_dict[key] = value.to_dict()
+        with open(self.__file_path, "w", encoding="UTF-8") as f:
+            json.dump(save_dict, f)
+
+    def reload(self):
+        try:
+            with open(self.__file_path, "r", encoding="UTF-8") as f:
+                obj = json.load(f)
+
+        except BaseException:
+            pass
