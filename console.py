25d035ada693884b26569ea85c592a1683adb928
Aymenbs22 <aymenbenslima1@gmail.com>
2022-03-06 10:37:22 -0800

    console

--- console.py
+++ console.py
@@ -5,6 +5,10 @@ from models.base_model import BaseModel
 from models.engine.file_storage import FileStorage
 from models import storage
 from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 
 
 class HBNBCommand(cmd.Cmd):
