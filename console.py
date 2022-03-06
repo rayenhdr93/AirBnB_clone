35274b9b5cfdfb14f5ddd6133d612a0591e97e26
rayenhdr93 <hedrirayen69@gmail.com>
2022-03-06 13:52:41 +0100

    create show destroy added

--- console.py
+++ console.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 """console"""
 import cmd
-
+from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage
+from models import storage
 
 class HBNBCommand(cmd.Cmd):
     """class HBNBCommand"""
-    prompt = "(hbnb)"
+    prompt = "(hbnb) "
 
     def do_EOF(self, line):
         """Quit command to exit the program"""
@@ -17,6 +19,50 @@ class HBNBCommand(cmd.Cmd):
 
     def emptyline(self):
         pass
+
+    def do_create(self, line):
+        if len(line) == 0:
+            print("** class name missing **")
+        elif line == "BaseModel":
+            new_ins = BaseModel()
+            new_ins.save()
+            print(new_ins.id)
+        else:
+            print("** class doesn't exist **")
+
+    def do_show(self, line):
+        list = line.split(" ")
+        if len(line) == 0:
+            print("** class name missing **")
+        else :
+            if list[0] not in("BaseModel", "FileStorage"):
+                print("** class doesn't exist **")
+            elif len(list) < 2:
+                print("** instance id missing **")
+            else :
+                ids = storage.all()
+                if (list[0]+ "." + list[1]) in ids.keys():
+                    print(ids[list[0]+ "." + list[1]])
+                else:
+                    print("** no instance found **")
+        
+    def do_destroy(self, line):
+        list = line.split(" ")
+        if len(line) == 0:
+            print("** class name missing **")
+        else :
+            if list[0] not in("BaseModel", "FileStorage"):
+                print("** class doesn't exist **")
+            elif len(list) < 2:
+                print("** instance id missing **")
+            else :
+                ids = storage.all()
+                if ids[list[0]+ "." + list[1]]:
+                    del(ids[list[0]+ "." + list[1]])
+                    storage.save()
+                else:
+                    print("** no instance found **")
+    
 
 
 if __name__ == '__main__':
